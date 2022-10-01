8a5210e813f6af86aba0d19ff5906a4113c5cb61
Abhi394394 <114810309+Abhi394394@users.noreply.github.com>
2022-10-01 17:03:13 +0530

    Update ExampleUnitTest.kt

--- app/src/test/java/com/abhishek/socialapp/ExampleUnitTest.kt
+++ app/src/test/java/com/abhishek/socialapp/ExampleUnitTest.kt
@@ -5,12 +5,7 @@ import org.junit.Test
 import org.junit.Assert.*
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
-    @Test
+ * Example local unit test, whic
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
